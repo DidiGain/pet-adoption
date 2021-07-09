c42f4a721e9a0990c32b4000a679472182130e83
Dinara Gainulina <dignara@yandex.ru>
2021-07-09 18:41:54 +0300

    feat: add show a pet wih photo logic

---
+++ src/Pet.js
@@ -0,0 +1,20 @@
+const Pet = ({ name, animal, breed, images, location }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
+  return (
+    <div className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Pet;
