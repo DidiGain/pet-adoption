09f934fe1df89e1e0767c38e0d09e2740c06af8b
Dinara Gainulina <dignara@yandex.ru>
2021-07-12 18:02:45 +0300

    feat: add error handler with redirect

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, redirect: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => {
+        this.setState({ redirect: true });
+      }, 5000);
+    }
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    } else if (this.state.hasError) {
+      return (
+        <h2>
+          This listing has an error. <Link to="/">Click here</Link> to go to the
+          home page OR wait 5 seconds to be redirected.
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
